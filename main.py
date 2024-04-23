1a5b7a806fb9991600e2f4a721ac7657f0533b06
kaladeens <sabd0044@student.monash.edu>
2024-04-23 14:39:31 +1000

    og main

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import cv2 
 import numpy as np
 from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture('vid.mp4')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
